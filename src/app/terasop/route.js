c6be99d53aaecfcc7ae5297bc139ab89938fd40d
sh20raj <shaswatraj3@gmail.com>
2024-07-18 09:58:31 +0530

    Support Multiple URLs

--- src/app/terasop/route.js
+++ src/app/terasop/route.js
@@ -1,12 +1,133 @@
 import { NextResponse } from "next/server";
 import TelegramBot from "node-telegram-bot-api";
 import CryptoJS from "crypto-js";
-import { getintotouch } from "./funcs";
 
 const token = process.env.TERASOP;
 
 const bot = new TelegramBot(token);
 const botlogger = "-1002221558664";
+
+
+async function download(url) {
+  try {
+    let id = url.split("/").pop();
+    console.log("The ID is " + id);
+    bot.sendChatAction(chatId, "typing");
+
+    try {
+      fetch(
+        "https://terabox.tech/api/upload?id=" + id + "&user=" + chatId
+      );
+    } catch (error) {}
+
+    // New API endpoint and request body
+    const apiUrl = "https://teraboxdownloader.in/api/video-downloader";
+    const encryptedLink = encryptString(url); // Assuming you have the encryptString function from earlier
+
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/javascript, */*; q=0.01",
+        "Accept-Language": "en-US,en;q=0.9,hi;q=0.8",
+        "Cache-Control": "no-cache",
+        "Content-Type": "application/json",
+        Pragma: "no-cache",
+        "Sec-Ch-Ua":
+          '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": '"macOS"',
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "X-Requested-With": "XMLHttpRequest",
+      },
+      referrer: `https://teraboxdownloader.in/video-downloader?link=${encodeURIComponent(
+        "https://terabox.com/s/" + id
+      )}`,
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: JSON.stringify({ link: encryptedLink }),
+      credentials: "include",
+    });
+
+    const data = await response.json();
+    console.log("Response:", data);
+
+    if (data.success && data.list.length > 0) {
+      const fileInfo = data.list[0];
+      const msgTemplate = `<b>Share this bot to keep this bot Working and Join @sopbots</b>\n<b>File Name:</b> ${
+        fileInfo.fileName
+      }\nOriginal Link: ${url}\n<b>File Size:</b> ${
+        fileInfo.fileSize
+      }\n<b>Uploaded At:</b> ${
+        fileInfo.uploadedAt
+      }\n<b>Download Link:</b> <a href="${
+        fileInfo.downloadLink
+      }">Download (if fast download not works)</a>\n<b>Watch Link:</b> <a href="https://teradl.shraj.workers.dev/?url=${encodeURIComponent(
+        fileInfo.downloadLink
+      )}">Watch</a>`;
+
+      const options = {
+        parse_mode: "HTML",
+        reply_markup: {
+          inline_keyboard: [
+            [
+              { text: "Fast Download", url: fileInfo.fastDownloadLink },
+              {
+                text: "Watch",
+                url: `https://codexdindia.blogspot.com/p/teradl.html?url=${encodeURIComponent(
+                  url
+                )}`,
+              },
+            ],
+          ],
+        },
+      };
+
+      bot.sendMessage(chatId, msgTemplate, options);
+
+      bot.sendMessage(botlogger, msgTemplate, options);
+
+      try {
+        let video = await fetch(
+          "https://imagehippoo.shraj.workers.dev/?url=" +
+            fileInfo.fastDownloadLink
+        );
+        let videoData = await video.json();
+        if (!videoData.data.view_url) {
+          videoData = await fetch(
+            "https://imagehippoo.shraj.workers.dev/?url=" +
+              fileInfo.downloadLink
+          );
+          videoData = await videoData.json();
+        }
+        bot.sendMessage(chatId, videoData.data.view_url);
+        bot.sendMessage(botlogger, videoData.data.view_url);
+      } catch (error) {
+        console.error("Error:", error);
+        bot.sendMessage(
+          chatId,
+          "https://codexdindia.blogspot.com/p/teradl.html?url=" +
+            encodeURIComponent(url)
+        );
+        bot.sendMessage(
+          botlogger,
+          "https://codexdindia.blogspot.com/p/teradl.html?url=" +
+            encodeURIComponent(url)
+        );
+        bot.sendMessage(chatId, fileInfo.fastDownloadLink);
+        bot.sendMessage(botlogger, fileInfo.fastDownloadLink);
+      }
+    } else {
+      bot.sendMessage(chatId, "No download links found.");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    bot.sendMessage(
+      chatId,
+      "An error occurred while processing your request"
+    );
+  }
+}
 
 export async function POST(req) {
   try {
@@ -43,129 +164,16 @@ export async function POST(req) {
 
     // Check if the message contains a link
     if (textContent.includes("https://")) {
-      const url = extractUrl(textContent);
-
+      const urls = extractUrl(textContent);
       // Check if the URL is from teraboxapp.com
-      if (url.includes("terabox")) {
-        try {
-          let id = url.split("/").pop();
-          console.log("The ID is " + id);
-          bot.sendChatAction(chatId, "typing");
-
-          try {
-            fetch(
-              "https://terabox.tech/api/upload?id=" + id + "&user=" + chatId
-            );
-          } catch (error) {}
-
-          // New API endpoint and request body
-          const apiUrl = "https://teraboxdownloader.in/api/video-downloader";
-          const encryptedLink = encryptString(url); // Assuming you have the encryptString function from earlier
-
-          const response = await fetch(apiUrl, {
-            method: "POST",
-            headers: {
-              Accept: "application/json, text/javascript, */*; q=0.01",
-              "Accept-Language": "en-US,en;q=0.9,hi;q=0.8",
-              "Cache-Control": "no-cache",
-              "Content-Type": "application/json",
-              Pragma: "no-cache",
-              "Sec-Ch-Ua":
-                '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-              "Sec-Ch-Ua-Mobile": "?0",
-              "Sec-Ch-Ua-Platform": '"macOS"',
-              "Sec-Fetch-Dest": "empty",
-              "Sec-Fetch-Mode": "cors",
-              "Sec-Fetch-Site": "same-origin",
-              "X-Requested-With": "XMLHttpRequest",
-            },
-            referrer: `https://teraboxdownloader.in/video-downloader?link=${encodeURIComponent(
-              "https://terabox.com/s/" + id
-            )}`,
-            referrerPolicy: "strict-origin-when-cross-origin",
-            body: JSON.stringify({ link: encryptedLink }),
-            credentials: "include",
-          });
-
-          const data = await response.json();
-          console.log("Response:", data);
-
-          if (data.success && data.list.length > 0) {
-            const fileInfo = data.list[0];
-            const msgTemplate = `<b>Share this bot to keep this bot Working and Join @sopbots</b>\n<b>File Name:</b> ${
-              fileInfo.fileName
-            }\nOriginal Link: ${url}\n<b>File Size:</b> ${
-              fileInfo.fileSize
-            }\n<b>Uploaded At:</b> ${
-              fileInfo.uploadedAt
-            }\n<b>Download Link:</b> <a href="${
-              fileInfo.downloadLink
-            }">Download (if fast download not works)</a>\n<b>Watch Link:</b> <a href="https://teradl.shraj.workers.dev/?url=${encodeURIComponent(
-              fileInfo.downloadLink
-            )}">Watch</a>`;
-
-            const options = {
-              parse_mode: "HTML",
-              reply_markup: {
-                inline_keyboard: [
-                  [
-                    { text: "Fast Download", url: fileInfo.fastDownloadLink },
-                    {
-                      text: "Watch",
-                      url: `https://codexdindia.blogspot.com/p/teradl.html?url=${encodeURIComponent(
-                        url
-                      )}`,
-                    },
-                  ],
-                ],
-              },
-            };
-
-            bot.sendMessage(chatId, msgTemplate, options);
-
-            bot.sendMessage(botlogger, msgTemplate, options);
-
-            try {
-              let video = await fetch(
-                "https://imagehippoo.shraj.workers.dev/?url=" +
-                  fileInfo.fastDownloadLink
-              );
-              let videoData = await video.json();
-              if (!videoData.data.view_url) {
-                videoData = await fetch(
-                  "https://imagehippoo.shraj.workers.dev/?url=" +
-                    fileInfo.downloadLink
-                );
-                videoData = await videoData.json();
-              }
-              bot.sendMessage(chatId, videoData.data.view_url);
-              bot.sendMessage(botlogger, videoData.data.view_url);
-            } catch (error) {
-              console.error("Error:", error);
-              bot.sendMessage(
-                chatId,
-                "https://codexdindia.blogspot.com/p/teradl.html?url=" +
-                  encodeURIComponent(url)
-              );
-              bot.sendMessage(
-                botlogger,
-                "https://codexdindia.blogspot.com/p/teradl.html?url=" +
-                  encodeURIComponent(url)
-              );
-              bot.sendMessage(chatId, fileInfo.fastDownloadLink);
-              bot.sendMessage(botlogger, fileInfo.fastDownloadLink);
-            }
-          } else {
-            bot.sendMessage(chatId, "No download links found.");
-          }
-        } catch (error) {
-          console.error("Error:", error);
-          bot.sendMessage(
-            chatId,
-            "An error occurred while processing your request"
-          );
-        }
+      if (urls) {
+        urls.forEach((url) => {
+          download(url);
+        });
       }
+
+      return NextResponse.json({}, { status: 200 });
+      
     } else if (textContent === "hi") {
       // Send "Hi" as a response
       bot.sendMessage(chatId, "Hi");
@@ -198,10 +206,11 @@ export const GET = () => {
 };
 
 // Function to extract URL from message or caption
-function extractUrl(text) {
-  const regex = /(https?:\/\/[^\s]+)/;
+function extractUrls(text) {
+  // Regular expression to match URLs with various formats
+  const regex = /(https?:\/\/[^\s]+)/g;
   const matches = text.match(regex);
-  return matches ? matches[0] : "";
+  return matches ? matches : [];
 }
 
 function encryptString(input) {
