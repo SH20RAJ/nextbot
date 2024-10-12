1ab321b0fa8c233beda745630e7569af54149b06
sh20raj <shaswatraj3@gmail.com>
2024-10-13 00:26:29 +0530

    Send all available links

--- src/app/terasop/funcs.js
+++ src/app/terasop/funcs.js
@@ -168,14 +168,45 @@ export async function downloadwithImage(url, chatId) {
       "https://player.terabox.tech/?url=https%3A%2F%2Fteraboxapp.com%2Fs%2F" +
       id;
 
-    bot.sendMessage(chatId, "WatchLink :- " + watchlink);
-    bot.sendMessage(botlogger, "WatchLink :- " + watchlink);
+    // bot.sendPhoto(
+    //   chatId,
+    //   "https://avatars.githubusercontent.com/u/66713844?v=4"
+    // );
+
+    let msg = `
+      Original Link :- ${" https://teraboxapp.com/s/"+id}
+
+
+WatchLink :- ${watchlink}
+
+      `;
+
+    bot.sendMessage(chatId, msg);
+    // bot.sendMessage(botlogger, "WatchLink :- " + watchlink);
 
     try {
       let api = "https://teraboxapi2.darkhacker7301.workers.dev/?url=" + url;
+      console.clear();
+      console.log("API :- " + api);
 
       let response = await fetch(api);
       let data = await response.json();
+
+      console.log("Data :- " + data);
+
+      console.log("Thumbnail :- " + data.response[0].thumbnail);
+      // bot.sendPhoto(chatId, data.response[0].thumbnail);
+
+      bot.sendMessage(
+        chatId,
+        `Title: <b>${data.response[0].title}</b>\n
+        Fast Download: <a href="${data.response[0].resolutions["Fast Download"]}">Fast Download Link</a>\n
+        HD Video: <a href="${data.response[0].resolutions["HD Video"]}">HD Video Link</a>\n
+        Thumbnail: <a href="${data.response[0].thumbnail}">Thumbnail Link</a>\n
+        \nOriginal Link :- ${url}\n
+        \nWatchLink :- ${watchlink}`,
+        { parse_mode: "HTML" }
+      );
 
       /*
         
@@ -192,101 +223,21 @@ export async function downloadwithImage(url, chatId) {
   ]
 }
         */
-
-      bot.sendPhoto(chatId, data.response[0].thumbnail, {
-        caption: `Title: ${data.response[0].title}
-  \nFast Download: ${data.response[0].resolutions["Fast Download"]}
-  \nHD Video: ${data.response[0].resolutions["HD Video"]}
-  \n\nWatch Link: ${watchlink}
-  \n\nShare this bot to keep this bot Working and Join @sopbots
-  \n\n Get App Access :- https://forms.gle/SWxyKYYxZyVLbcQ77 (After filling the form you will get the app access link given below)
-  \n Download App :- https://play.google.com/store/apps/details?id=co.median.android.nydowl (open in chrome for download)
-  `,
-      });
-
-      bot.sendVideo(chatId, data.response[0].resolutions["Fast Download"]);
-      bot.sendVideo(chatId, data.response[0].resolutions["HD Video"]);
-    } catch (error) {}
-
-    try {
-      fetch("https://terabox.tech/api/upload?id=" + id + "&user=" + chatId);
-    } catch (error) {}
-
-    // New API endpoint and request body
-    const apiUrl = "https://teraboxdownloader.in/api/video-downloader";
-    const encryptedLink = encryptString(url); // Assuming you have the encryptString function from earlier
-
-    const response = await fetch(apiUrl, {
-      method: "POST",
-      headers: {
-        Accept: "application/json, text/javascript, */*; q=0.01",
-        "Accept-Language": "en-US,en;q=0.9,hi;q=0.8",
-        "Cache-Control": "no-cache",
-        "Content-Type": "application/json",
-        Pragma: "no-cache",
-        "Sec-Ch-Ua":
-          '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-        "Sec-Ch-Ua-Mobile": "?0",
-        "Sec-Ch-Ua-Platform": '"macOS"',
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-origin",
-        "X-Requested-With": "XMLHttpRequest",
-      },
-      referrer: `https://teraboxdownloader.in/video-downloader?link=${encodeURIComponent(
-        "https://terabox.com/s/" + id
-      )}`,
-      referrerPolicy: "strict-origin-when-cross-origin",
-      body: JSON.stringify({ link: encryptedLink }),
-      credentials: "include",
-    });
-
-    const data = await response.json();
-    console.log("Response:", data);
-
-    const response2 = await fetch(
-      "https://ytshorts.savetube.me/api/v1/terabox-downloader",
-      {
-        headers: {
-          accept: "application/json, text/plain, */*",
-          "accept-language": "en-US,en;q=0.9,hi;q=0.8",
-          "cache-control": "no-cache",
-          "content-type": "application/json",
-          pragma: "no-cache",
-          priority: "u=1, i",
-          "sec-ch-ua":
-            '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": '"macOS"',
-          "sec-fetch-dest": "empty",
-          "sec-fetch-mode": "cors",
-          "sec-fetch-site": "same-origin",
-        },
-        referrerPolicy: "no-referrer",
-        method: "POST",
-        mode: "cors",
-        credentials: "include",
-        body: JSON.stringify({ url: url }),
-      }
-    );
-
-    const data2 = await response2.json();
-
-    if (data.success && data.list.length > 0) {
-      const fileInfo = data.list[0];
+      // bot.sendMessage(chatId, "sending messsssage");
 
       const msgTemplate = `<b>Share this bot Using /share </b>
-      \n<b>File Name:</b> ${fileInfo.fileName}
-      \nOriginal Link: ${url}
-      \n<b>File Size:</b> ${fileInfo.fileSize}
-      \n<b>Uploaded At:</b> ${fileInfo.uploadedAt}
-      \n<b>Download Link:</b> <a href="${fileInfo.downloadLink}">Download (if fast download not works)</a>\n<b>Watch Link:</b>
-      \n<a href="${watchlink}">Watch</a>
-      \n<b>Share this bot to keep this bot Working and Join @sopbots</b>
-      \n Watch Link: ${watchlink}
-      \n Get App Access :- https://forms.gle/SWxyKYYxZyVLbcQ77 (After filling the form you will get the app access link given below)
-      \n Download App :- https://play.google.com/store/apps/details?id=co.median.android.nydowl (open in chrome for download)
-      `;
+\n<b>File Name:</b> ${data.response[0].title}
+\nOriginal Link: ${url}
+\n<b>File Size:</b> ${data.response[0].fileSize}
+  \n<b>Download Link:</b> <a href="${data.response[0].resolutions["Fast Download"]}">Download (if fast download not works)</a>\n<b>Watch Link:</b>
+\n<a href="${watchlink}">Watch</a>
+\n<b>Share this bot to keep this bot Working and Join @sopbots</b>
+\n Watch Link: ${watchlink}
+\n Get App Access :- https://forms.gle/SWxyKYYxZyVLbcQ77 (After filling the form you will get the app access link given below)
+\n Download App :- https://play.google.com/store/apps/details?id=co.median.android.nydowl (open in chrome for download)
+`;
+
+      console.log("MsgTemplate :- " + msgTemplate);
 
       const options = {
         parse_mode: "HTML",
@@ -308,72 +259,27 @@ export async function downloadwithImage(url, chatId) {
         },
       };
 
-      bot.sendPhoto(chatId, data2.response[0].thumbnail, {
-        caption: msgTemplate,
-        ...options,
-      });
+      // bot.sendPhoto(chatId, data.response[0].thumbnail, {
+      //   caption: msgTemplate,
+      //   ...options,
+      // });
 
-      bot.sendPhoto(botlogger, data2.response[0].thumbnail, {
-        caption: msgTemplate,
-        ...options,
-      });
+      bot.sendMessage(chatId, "sending message");
 
-      // bot.sendMessage("1479193538",  "before https://phpbot.sh20raj.com/api/video.php?url=" + encodeURIComponent(fileInfo.fastDownloadLink));
+      bot.sendMessage(chatId, msgTemplate, options);
 
-      try {
-        bot.sendVideo(chatId, fileInfo.fastDownloadLink);
-        bot.sendVideo(botlogger, fileInfo.fastDownloadLink);
-        // bot.sendVideo(chatId,  "https://phpbot.sh20raj.com/api/video.php?url=" + encodeURIComponent(fileInfo.fastDownloadLink));
-        // bot.sendVideo(botlogger,  "https://phpbot.sh20raj.com/api/video.php?url=" + encodeURIComponent(fileInfo.fastDownloadLink));
-        // bot.sendMessage("1479193538",  "top https://phpbot.sh20raj.com/api/video.php?url=" + encodeURIComponent(fileInfo.fastDownloadLink));
-      } catch (error) {
-        bot.sendVideo(
-          chatId,
-          "https://phpbot.sh20raj.com/api/video.php?url=" +
-            encodeURIComponent(fileInfo.fastDownloadLink)
-        );
-        bot.sendVideo(
-          botlogger,
-          "https://phpbot.sh20raj.com/api/video.php?url=" +
-            encodeURIComponent(fileInfo.fastDownloadLink)
-        );
-        // bot.sendMessage("1479193538",  "https://phpbot.sh20raj.com/api/video.php?url=" + encodeURIComponent(fileInfo.fastDownloadLink));
-      }
+      bot.sendMessage(botlogger, "sentmessage");
 
-      // bot.sendMessage(
-      //   chatId,
-      //   `✨ Access to Preview Version of Premium @TeraSop_bot (Video Player Online) \n Use /share and share to 5 of your friends to keep the video watching feature on after the access ends  :- https://codexdindia.blogspot.com/p/terabox-downloader.html?url=${url}`
-      // );
+      // bot.sendVideo(chatId, data.response[0].resolutions["Fast Download"]);
+      // bot.sendVideo(chatId, data.response[0].resolutions["HD Video"]);
+    } catch (error) {}
 
-      // promote the website https://www.terabox.tech/  for downloading, viewing and enbedding the videos with no limits
-
-      bot.sendMessage(
-        chatId,
-        `Visit https://www.terabox.tech/ for downloading, viewing and embedding the videos with no filesize, bandwidth, etc limits 🚀 
-        Download Chrome Extension for Fast Downloading and Embedding the Videos in your Website 🚀
-
-        \nLink to Your Video :- ${watchlink}`
-      );
-
-      // send message that you can earn money by sharing the bot with your friends or social media for more info use /share
-
-      // bot.sendMessage(
-      //   chatId,
-      //   `🎉 Share this link to your friends and get benefits for each friend who joins using your link! 🎉 \n\n Your Referral Link is below, Share this to your friends or on social media to earn money 💸 ($1 or ₹100 / 1k Users) \n https://t.me/terasop_bot?start=${chatId}  \n\n. Check => /share 🎶`,
-      //   { disable_web_page_preview: true }
-      // );
-    } else {
-      // bot.sendMessage(chatId, "No download links found.");
-    }
+    // try {
+    //   fetch("https://terabox.tech/api/upload?id=" + id + "&user=" + chatId);
+    // } catch (error) {}
   } catch (error) {
     console.error("Error:", error);
-    bot.sendMessage(
-      chatId,
-      "An error occurred while processing your request if its a correct link try resending it 🚀. Otherwise you can report on @sopbots"
-    );
-  } finally {
-    bot.sendChatAction(chatId, "typing");
-    // main();
+    bot.sendMessage(chatId, "An error occurred while processing your request");
   }
 }
 
